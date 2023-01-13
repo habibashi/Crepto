e695a068922d3779445c5e39a79c5fc66200a6b5
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-01-13 04:23:17 +0200

    app express server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+// const connectDB = require("./config/db");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => console.log(`server started on port ${port.yellow}`));
