14288ee87ad8e721f1929b12135a9d959736eec2
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-01-14 22:22:50 +0200

    add charts and search page and complete register and login work

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Main from "../components/Main";
 import SideBar from "../components/SideBar";
+import "react-toastify/dist/ReactToastify.css"
 
 const Home = () => {
+    const navigate = useNavigate();
+    const { user } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }, [user, navigate]);
+
     return (
         <div className="flex bg-bodyBg">
             <SideBar />
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,33 +1,63 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 const Register = () => {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
-    confirm_password: "",
+    confirmPassword: "",
   });
 
-  const { username, password, confirm_password } = formData;
+  const { username, password, confirmPassword } = formData;
 
-  const onChange = (event) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChangeHandler = (event) => {
     setFormData((prevState) => ({
       ...prevState,
       [event.target.name]: event.target.value,
     }));
   };
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
+
+    dispatch(register(formData));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <div className="bg-logInBG flex justify-center h-screen">
-      <div className="rounded-[7px] shadow-2xl w-[450px] h-max mt-20 bg-inside p-7">
+      <div className="rounded-[10px] shadow-2xl w-[450px] h-max mt-20 bg-inside p-7">
         <h3 className="text-white text-center font-bold text-3xl">Register</h3>
         <form onSubmit={onSubmitHandler}>
           <div className="mb-6">
             <label
-              for="username"
+              htmlFor="username"
               className="w-max block mb-2 text-sm font-medium text-white"
             >
               Username
@@ -40,13 +70,13 @@ const Register = () => {
               className="border text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
               placeholder="username"
               value={username}
-              onChange={onChange}
+              onChange={onChangeHandler}
               required
             />
           </div>
           <div className="mb-6">
             <label
-              for="password"
+              htmlFor="password"
               className="w-max block mb-2 text-sm font-medium text-white"
             >
               Password
@@ -58,13 +88,13 @@ const Register = () => {
               className="border text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
               placeholder="password"
               value={password}
-              onChange={onChange}
+              onChange={onChangeHandler}
               required
             />
           </div>
           <div className="mb-6">
             <label
-              for="confirm_password"
+              htmlFor="confirm_password"
               className="block mb-2 text-sm font-medium w-max text-white"
             >
               Confirm password
@@ -72,36 +102,19 @@ const Register = () => {
             <input
               type="password"
               id="confirm_password"
-              name="confirm_password"
+              name="confirmPassword"
               className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
               placeholder="Confirm password"
-              value={confirm_password}
-              onChange={onChange}
+              value={confirmPassword}
+              onChange={onChangeHandler}
               required
             />
-          </div>
-          <div className="flex items-start mb-6">
-            <div className="flex items-center h-5">
-              <input
-                id="remember"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 border rounded focus:ring-3bg-gray-700border-gray-600 focus:ring-blue-600 ring-offset-gray-800"
-                required
-              />
-            </div>
-            <label
-              for="remember"
-              className="w-max ml-2 text-sm font-medium text-gray-300"
-            >
-              I agree.
-            </label>
           </div>
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Submit
+            Register
           </button>
           <h2 className="text-blue-800 text-center cursor-pointer">
             <Link to="/login">Switch to login</Link>
