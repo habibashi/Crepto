7d1af09ece4743f698ab11d9dc97e3bc24530592
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-01-14 22:26:46 +0200

    server check

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
-// const connectDB = require("./config/db");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -11,8 +9,6 @@ connectDB();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
