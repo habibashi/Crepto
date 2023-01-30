191a3bb4f1d978ebe5e66ade88fb36a7ccf70cc1
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-01-30 05:01:50 +0200

    getCoins and searchCoins and chartsCoins api

--- frontend/src/features/API/fetchAPI.js
+++ frontend/src/features/API/fetchAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://api.coingecko.com/api/v3";
 
-export const getCoins = async () => {
+export const getCoins = async (page) => {
   const response = await axios.get(
     BASE_URL +
       "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=7d"
@@ -25,6 +25,29 @@ export const getCoins = async () => {
       });
     }
     return coinData;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const searchCoin = async (searchInput) => {
+  const response = await axios.get(BASE_URL + `/search?query=${searchInput}`);
+
+  try {
+    return response.data.coins;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const chartsCoin = async (id) => {
+  const response = await axios.get(
+    BASE_URL + `/coins/${id}/market_chart?vs_currency=usd&days=7&interval=daily`
+  );
+  try {
+    return response.data.prices;
   } catch (error) {
     console.log(error);
     return error;
