23d182097df812a0f3f61c77002449692a6255bc
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-01-30 05:05:07 +0200

    new pakages and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,25 @@ import { ToastContainer } from "react-toastify";
 import LogIn from "./pages/LogIn";
 import Register from "./pages/Register";
 import NotFound from "./components/NotFound";
+import Charts from "./pages/Charts";
+import Search from "./pages/Search";
+import ShowCoins from "./components/ShowCoins";
 import "react-toastify/dist/ReactToastify.css";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" element={<LogIn />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />}>
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="/home" element={<ShowCoins />} />
+          <Route path="/chart/:id" element={<Charts />} />
+          <Route path="/search" element={<Search />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
       <ToastContainer />
