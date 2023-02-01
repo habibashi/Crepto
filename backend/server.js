9fd376c7c7a9ca07a0ab0ab49d697428675d4390
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-02-01 23:48:05 +0200

    create the url for trade

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -14,5 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/trade", require("./routes/tradeRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port.yellow}`));
